d130f37d55cca49a659ed3ecab3f417fd2942aaa
Omar Naguib <55961349+OmarNaguib@users.noreply.github.com>
2024-02-20 02:40:39 +0200

    Update grid layout in App component

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ const ResponsiveGridLayout = WidthProvider(Responsive);
 
 function App() {
   const [layout, setLayout] = useState([
-    { i: "a", x: 0, y: 0, w: 5, h: 1 },
-    { i: "b", x: 5, y: 0, w: 5, h: 1 },
-    { i: "c", x: 2, y: 1, w: 5, h: 1 },
-    { i: "d", x: 0, y: 2, w: 5, h: 1 },
-    { i: "f", x: 5, y: 2, w: 5, h: 1 },
-    { i: "g", x: 2, y: 3, w: 5, h: 1 },
+    { i: "a", x: 0, y: 0, w: 6, h: 1 },
+    { i: "b", x: 6, y: 0, w: 6, h: 1 },
+    { i: "c", x: 2, y: 1, w: 12, h: 1 },
+    { i: "d", x: 0, y: 2, w: 6, h: 1 },
+    { i: "f", x: 6, y: 2, w: 6, h: 1 },
+    { i: "g", x: 2, y: 3, w: 6, h: 1 },
   ]);
 
   const onLayoutChange = (newLayout) => {
@@ -58,7 +58,7 @@ function App() {
         className="layout"
         layout={layout}
         breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-        cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+        cols={{ lg: 12, md: 12, sm: 12, xs: 12, xxs: 12 }}
         rowHeight={400}
         onLayoutChange={onLayoutChange}
         draggableHandle=".dragHandle"
